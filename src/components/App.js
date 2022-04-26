3a885ef2914a7f2e27404ab6d6fba8e33c2921f8
saravanan m <msaravanan1998@gmail.com>
2022-04-26 18:48:22 +0530

    update app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
 
+
 const App = () => {
-  return (
-    <div id="main"></div>
-  )
-}
+  return <div id="main">
+    <h1 data-ns-test="project-name">Memories Website</h1>
+    <h2 data-ns-test="project-description">A website for my favourite memories.</h2>
+  </div>;
+};
 
 
 export default App;
